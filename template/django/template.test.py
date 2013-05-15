a9b32f23865f712bfc73b2b95cb3d838c5d3db79
kf <7kfpun@gmail.com>
2013-05-16 02:07:04 +0800

    add django templates

---
+++ template/django/template.test.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+
+class BaseTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_response(self):
+        response = self.client.et('')
+        self.assertEqual(response.status_code, 200)
